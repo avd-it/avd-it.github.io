aac319edf6d8b296c1b483aabacb17388f69b970
alvandijk <95425356+alvandijk@users.noreply.github.com>
2021-12-02 17:42:50 +0100

    commit 2

---
+++ snippet1.ps1
@@ -0,0 +1,26 @@
+﻿
+
+try {  
+    #Importeer de teams uit een csv-bestand
+    $Teams = import-csv -LiteralPath ".\Teams.csv"
+
+    
+    foreach($Team in $Teams) {  
+        $teamName = $Team.Naam  
+        $teamDescription = $Team.Omschrijving
+        $teamOwner = $Team.Eigenaar  
+        $teamVisibility = $Team.Zichtbaaarheid  
+        #create the team with specified parameters  
+        #$groupID = New-Team -DisplayName $teamName -Owner $teamOwner -Description $teamDescription -Visibility $teamVisibility  
+        Write-host $Team.Naam
+        Write-host $Team.Omschrijving
+        Write-host $Team.Eigenaar
+        Write-host $Team.Zichtbaarheid
+        Write-Host "Team " $teamName " succesvol aangemaakt..."  
+    }  
+    Write-Host $Teams.Count " teams were created" - ForegroundColor Green - BackgroundColor Black  
+} 
+catch {  
+    Write-Host "An error occurred:"  
+    Write-Host $_  
+}  
